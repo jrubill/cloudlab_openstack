cc1b4d83fdff8162142010dc823f02e0434b009f
Jeff <jrubill@g.clemson.edu>
2018-02-19 10:33:00 -0500

    Assigned IP addresses to the nodes

--- setup-controller.sh
+++ setup-controller.sh
@@ -4468,11 +4468,13 @@ flavor_id=`openstack flavor list -f value | grep m1.small | cut -d' ' -f 1`
 image_id=`openstack image list -f value | grep OL7 | cut -d' ' -f 1`
 security_id=`openstack security group list -f value | grep $project_id | cut -d' ' -f 1`
 port_id=`openstack port list -f value | grep testport3 | cut -d' ' -f 1`
+port_id1=`openstack port list -f value | grep testport2 | cut -d' ' -f 1`
+port_id2=`openstack port list -f value | grep testport1 | cut -d' ' -f 1`
 
 # See https://docs.openstack.org/mitaka/install-guide-ubuntu/launch-instance-selfservice.html
 openstack server create --flavor m1.medium --security-group $security_id --image OL7 --nic port-id=$port_id headnode
-openstack server create --flavor m1.medium --security-group $security_id --image OL7 --nic port-id=$port_id subnode1
-openstack server create --flavor m1.medium --security-group $security_id --image OL7 --nic port-id=$port_id subnode2
+openstack server create --flavor m1.medium --security-group $security_id --image OL7 --nic port-id=$port_id1 subnode1
+openstack server create --flavor m1.medium --security-group $security_id --image OL7 --nic port-id=$port_id2 subnode2
 
 
 
