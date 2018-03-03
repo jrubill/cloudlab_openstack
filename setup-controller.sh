0f35e02065c2d41b5d04d7f32636f9209d486745
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2018-03-03 13:50:40 -0500

    Update setup-controller.sh

--- setup-controller.sh
+++ setup-controller.sh
@@ -4456,6 +4456,7 @@ subnet_id=`openstack network show -f shell flat-lan-1-net | grep "^subnets=" | c
 openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-address=10.11.10.21 testport1
 openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-address=10.11.10.22 testport2
 openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-address=10.11.10.23 testport3
+openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-address=10.11.10.24 testport4
 
 # See https://docs.openstack.org/project-install-guide/baremetal/draft/configure-glance-images.html
 # wget -O /tmp/setup/OL7.vmdk https://clemson.box.com/shared/static/5dukzod4ftj9v3g5r8q0ktxzweuj2vvw.vmdk
@@ -4467,14 +4468,16 @@ project_id=`openstack project list -f value | grep admin | cut -d' ' -f 1`
 flavor_id=`openstack flavor list -f value | grep m1.small | cut -d' ' -f 1`
 image_id=`openstack image list -f value | grep OL7 | cut -d' ' -f 1`
 security_id=`openstack security group list -f value | grep $project_id | cut -d' ' -f 1`
-port_id=`openstack port list -f value | grep testport3 | cut -d' ' -f 1`
-port_id1=`openstack port list -f value | grep testport2 | cut -d' ' -f 1`
-port_id2=`openstack port list -f value | grep testport1 | cut -d' ' -f 1`
+port_id=`openstack port list -f value | grep testport4 | cut -d' ' -f 1`
+port_id1=`openstack port list -f value | grep testport3 | cut -d' ' -f 1`
+port_id2=`openstack port list -f value | grep testport2 | cut -d' ' -f 1`
+port_id3=`openstack port list -f value | grep testport1 | cut -d' ' -f 1`
 
 # See https://docs.openstack.org/mitaka/install-guide-ubuntu/launch-instance-selfservice.html
 openstack server create --flavor m1.medium --security-group $security_id --image OL7 --nic port-id=$port_id headnode
 openstack server create --flavor m1.medium --security-group $security_id --image OL7 --nic port-id=$port_id1 subnode1
 openstack server create --flavor m1.medium --security-group $security_id --image OL7 --nic port-id=$port_id2 subnode2
+openstack server create --flavor m1.medium --security-group $security_id --image OL7 --nic port-id=$port_id3 subnode3
 
 
 
