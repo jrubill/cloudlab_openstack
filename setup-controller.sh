a910d899488fc274db452650549cfbee41b4063f
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2018-03-06 23:12:51 -0500

    Update setup-controller.sh

--- setup-controller.sh
+++ setup-controller.sh
@@ -4453,10 +4453,10 @@ network_id=`openstack network show -f shell flat-lan-1-net | grep "^id=" | cut -
 subnet_id=`openstack network show -f shell flat-lan-1-net | grep "^subnets=" | cut -d'"' -f 2`
 
 # See https://docs.openstack.org/python-openstackclient/pike/cli/command-objects/port.html
-openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-address=10.11.10.21 testport1
-openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-address=10.11.10.22 testport2
-openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-address=10.11.10.23 testport3
-openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-address=10.11.10.24 testport4
+openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-address=192.168.2.301 testport1
+openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-address=192.168.2.302 testport2
+openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-address=192.168.2.303 testport3
+openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-address=192.168.2.304 testport4
 
 # See https://docs.openstack.org/project-install-guide/baremetal/draft/configure-glance-images.html
 # wget -O /tmp/setup/OL7.vmdk https://clemson.box.com/shared/static/5dukzod4ftj9v3g5r8q0ktxzweuj2vvw.vmdk
